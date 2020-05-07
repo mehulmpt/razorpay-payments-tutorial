8141a0d6435b9fa648725a40fcee92fa83820546
Mehul Mohan <mehulmpt@gmail.com>
2020-05-08 03:39:04 +0530

    Razorpay backend payment ready

---
+++ backend/index.js
@@ -0,0 +1,71 @@
+const app = require('express')()
+const path = require('path')
+const shortid = require('shortid')
+const Razorpay = require('razorpay')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const razorpay = new Razorpay({
+	key_id: 'rzp_test_uGoq5ABJztRAhk',
+	key_secret: 'FySe2f5fie9hij1a5s6clk9B'
+})
+
+app.get('/logo.svg', (req, res) => {
+	res.sendFile(path.join(__dirname, 'logo.svg'))
+})
+
+app.post('/verification', (req, res) => {
+	// do a validation
+	const secret = '12345678'
+
+	console.log(req.body)
+
+	const crypto = require('crypto')
+
+	const shasum = crypto.createHmac('sha256', secret)
+	shasum.update(JSON.stringify(req.body))
+	const digest = shasum.digest('hex')
+
+	console.log(digest, req.headers['x-razorpay-signature'])
+
+	if (digest === req.headers['x-razorpay-signature']) {
+		console.log('request is legit')
+		// process it
+		require('fs').writeFileSync('payment1.json', JSON.stringify(req.body, null, 4))
+	} else {
+		// pass it
+	}
+	res.json({ status: 'ok' })
+})
+
+app.post('/razorpay', async (req, res) => {
+	const payment_capture = 1
+	const amount = 499
+	const currency = 'INR'
+
+	const options = {
+		amount: amount * 100,
+		currency,
+		receipt: shortid.generate(),
+		payment_capture
+	}
+
+	try {
+		const response = await razorpay.orders.create(options)
+		console.log(response)
+		res.json({
+			id: response.id,
+			currency: response.currency,
+			amount: response.amount
+		})
+	} catch (error) {
+		console.log(error)
+	}
+})
+
+app.listen(1337, () => {
+	console.log('Listening on 1337')
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function loadScript(src) {
+	return new Promise((resolve) => {
+		const script = document.createElement('script')
+		script.src = src
+		script.onload = () => {
+			resolve(true)
+		}
+		script.onerror = () => {
+			resolve(false)
+		}
+		document.body.appendChild(script)
+	})
+}
+
+const __DEV__ = document.domain === 'localhost'
+
+function App() {
+	const [name, setName] = useState('Mehul')
+
+	async function displayRazorpay() {
+		const res = await loadScript('https://checkout.razorpay.com/v1/checkout.js')
+
+		if (!res) {
+			alert('Razorpay SDK failed to load. Are you online?')
+			return
+		}
+
+		const data = await fetch('http://localhost:1337/razorpay', { method: 'POST' }).then((t) =>
+			t.json()
+		)
+
+		console.log(data)
+
+		const options = {
+			key: __DEV__ ? 'rzp_test_uGoq5ABJztRAhk' : 'PRODUCTION_KEY',
+			currency: data.currency,
+			amount: data.amount.toString(),
+			order_id: data.id,
+			name: 'Donation',
+			description: 'Thank you for nothing. Please give us some money',
+			image: 'http://localhost:1337/logo.svg',
+			handler: function (response) {
+				alert(response.razorpay_payment_id)
+				alert(response.razorpay_order_id)
+				alert(response.razorpay_signature)
+			},
+			prefill: {
+				name,
+				email: 'sdfdsjfh2@ndsfdf.com',
+				phone_number: '9899999999'
+			}
+		}
+		const paymentObject = new window.Razorpay(options)
+		paymentObject.open()
+	}
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					onClick={displayRazorpay}
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Donate $5
+				</a>
+			</header>
+		</div>
+	)
+}
+
+export default App
